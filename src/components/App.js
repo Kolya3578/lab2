26b3d0b069e9213deb2fc7233a99e1e05972a6ed
Kolya3578 <koliankolea@gmail.com>
2024-11-08 17:59:31 +0200

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,9 @@ function App() {
   const onClickIncrease = () => {
     setCount(count + 1);
   };
-
   const onClickDecrease = () => {
     setCount(count - 1);
   };
-
   return (
     <div className="app">
       <div>
