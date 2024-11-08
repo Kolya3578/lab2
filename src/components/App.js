88c4b7ed21349d6b367f178b6087edbb6b9b6c5e
Kolya3578 <koliankolea@gmail.com>
2024-11-08 17:33:23 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const onClickDecrease = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h1>React</h1>
+        <h2>Score:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickIncrease} className="increase">+</button>
+        <button onClick={onClickDecrease} className="decrease">-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/comp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Comp() {
+   return <h1>Components</h1>
+}
+
+export default Comp;
